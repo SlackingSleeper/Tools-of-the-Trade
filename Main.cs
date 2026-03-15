2813bc27f62dcdc9e0c5ee9fd9f2e5994e5870dc
Mark Westerlund <mana.torsk@gmail.com>
2026-03-15 16:19:40 +0100

    Cleanup & formatting

--- Main.cs
+++ Main.cs
@@ -21,15 +21,8 @@ namespace ToolsOfTheTrade
             }
             try
             {
-
-                PatchGame();
-                /*Game.OnLevelLoadComplete += OnLevelLoadComplete;
-                 *if (RM.drifter)
-                 *{
-                 *  OnLevelLoadComplete();
-                 *}
-                */
                 Settings.Register();
+                PatchGame();
             }
             catch (Exception e)
             {
@@ -42,13 +35,6 @@ namespace ToolsOfTheTrade
         {
             HarmonyLib.Harmony harmony = new("Slacking.ToolsOfTheTrade");
             harmony.PatchAll();
-            //patch stuff
         }
-        /*private void OnLevelLoadComplete()
-        {
-            //game objects are cleared when the scene changes by default
-            //this function is called when a level is loaded so that you
-            //dont have to worry about your mods affecting the menus
-        }*/
     }
 }
