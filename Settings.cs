6fa13c28953f4bde7bb622f038b14e5c24d17c34
Mark Westerlund <mana.torsk@gmail.com>
2026-03-23 11:58:13 +0100

    Moved most settings out to modules

--- Settings.cs
+++ Settings.cs
@@ -4,27 +4,7 @@ namespace ToolsOfTheTrade
 {
     internal class Settings
     {
-        public static MelonPreferences_Category Selection;
-
-        public static MelonPreferences_Entry<bool> MineLayer;
-        public static MelonPreferences_Entry<bool> AirZooka;
-        public static MelonPreferences_Entry<bool> AirZookaDebug;
-        public static MelonPreferences_Entry<bool> SpinAttack;
-        public static MelonPreferences_Entry<bool> SwapBoof;
-
-        public static void Register()
-        {
-            Selection = MelonPreferences.CreateCategory("Selection");
-
-            MineLayer = Selection.CreateEntry("MineLayer", true);
-            AirZooka = Selection.CreateEntry("AirZooka", true);
-            AirZookaDebug = Selection.CreateEntry("AirZookaDebug", false);
-            SpinAttack = Selection.CreateEntry("SpinAttack", true);
-            //weaponName = Selection.CreateEntry("weaponName", true);
-            //weaponNameDebug = Selection.CreateEntry("weaponNameDebug", false);
-
-
-        }
-
+        public static readonly string mainCategoryName = "Tools of the Trade";
+        public static readonly string mainCategoryDebug = "Tools of the Trade/Debug";
     }
 }
