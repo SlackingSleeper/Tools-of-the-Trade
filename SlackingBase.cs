45ef8c6927ff5cbe6f8c67c02919c5e3508e012a
Mark Westerlund <mana.torsk@gmail.com>
2026-03-23 11:51:21 +0100

    Created framework

---
+++ SlackingBase.cs
@@ -0,0 +1,14 @@
+﻿
+using MelonLoader;
+
+namespace ToolsOfTheTrade
+{
+    internal abstract class SlackingBase
+    {
+        public abstract void RegisterSettings();
+        public abstract void Patch();
+        public virtual void OnPreferencesSaved()
+        {
+        }
+    }
+}
